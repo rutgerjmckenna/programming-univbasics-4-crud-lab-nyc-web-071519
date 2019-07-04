5815e5b30e178d14cf372c7d5d6e98c34adcfdc9
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-04 01:10:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["cat", "dog", "frog"]
+  array = 
   index_number = array[1]
 end
 
